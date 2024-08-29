a264c8fffb0ac1a77680e3371e5a1fcaa897875b
Toshiki Nishihata <nishihata@aicro.jp>
2024-08-30 01:23:23 +0900

    :white_check_mark: README.mdを追加

---
+++ README.md
@@ -0,0 +1,33 @@
+## download_backlog_attachments.sh
+
+指定されたBacklogプロジェクトIDに関連する全ての課題から添付ファイルをダウンロードするスクリプトです。
+各課題に関連する添付ファイルをローカルに保存します。
+
+## 事前に必要なコマンド:
+- jq
+- curl
+
+## 使用方法:
+
+```bash
+$ chmod +x download_backlog_attachments.sh
+$ ./download_backlog_attachments.sh -k YOUR_API_KEY -s YOUR_SPACE_ID -p YOUR_PROJECT_ID
+```
+
+## オプションの説明:
+
+**-k: Backlog APIキー**
+
+Backlogにログインし、以下のページからAPIキーを取得してください。
+https://hlm.backlog.com/EditApiSettings.action
+
+**-s: BacklogスペースID**
+
+スペースIDは、BacklogのURLに含まれる文字列です。
+例: https://[スペースID].backlog.com
+
+**-p: ダウンロード対象のプロジェクトID**
+
+プロジェクトIDは、プロジェクト設定のURLに含まれる数字です。
+例: https://hlm.backlog.com/ViewPermission.action?projectId=[プロジェクトID]
+
--- download_backlog_attachments.sh
+++ download_backlog_attachments.sh
@@ -3,25 +3,26 @@
 # 指定されたBacklogプロジェクトIDに関連する全ての課題から添付ファイルをダウンロードするスクリプトです。
 # 各課題に関連する添付ファイルをローカルに保存します。
 #
-# 必要なコマンド:
-#   - jq: JSONデータをパースするために使用します。インストールされていない場合は、事前にインストールしてください。
-#   - curl: HTTPリクエストを送信するために使用します。インストールされていない場合は、事前にインストールしてください。
+# 事前に必要なコマンド:
+#   - jq
+#   - curl
 #
 # 使用方法:
-#   ./download_backlog_attachments.sh -k YOUR_API_KEY -s YOUR_SPACE_ID -p YOUR_PROJECT_ID
+#   $ chmod +x download_backlog_attachments.sh
+#   $ ./download_backlog_attachments.sh -k YOUR_API_KEY -s YOUR_SPACE_ID -p YOUR_PROJECT_ID
 #
-# オプション:
+# オプションの説明:
 #   -k: Backlog APIキー
+#       Backlogにログインし、以下のページからAPIキーを取得してください。
+#       https://hlm.backlog.com/EditApiSettings.action
+#
 #   -s: BacklogスペースID
 #       スペースIDは、BacklogのURLに含まれる文字列です。
 #       例: https://[スペースID].backlog.com
+#
 #   -p: ダウンロード対象のプロジェクトID
 #       プロジェクトIDは、プロジェクト設定のURLに含まれる数字です。
 #       例: https://hlm.backlog.com/ViewPermission.action?projectId=[プロジェクトID]
-#
-# Backlog APIキーの取得方法:
-#   Backlogにログインし、以下のページからAPIキーを取得してください。
-#   https://hlm.backlog.com/EditApiSettings.action
 
 SPACE_DIR=$(pwd)
 COUNT=100
